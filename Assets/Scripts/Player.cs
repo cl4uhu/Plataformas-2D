c36672c4e86d0e55f2b291da7f899edb2f3d0433
cl4uhu <pjclaudia1@gmail.com>
2023-09-13 13:14:57 +0200

    movement and scene

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]private float _playerSpeed = 5;
+    private float _playerInputHorizontal; 
+    private float _playerInputVertical;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement ();  
+    }
+
+    void PlayerMovement()
+    { 
+      _playerInputHorizontal = Input.GetAxis("Horizontal");
+      _playerInputVertical = Input.GetAxis("Vertical");
+        
+      transform.Translate(new Vector2(_playerInputHorizontal, _playerInputVertical) * _playerSpeed * Time.deltaTime); 
+    }
+}
