99f1a3332a3fc8b57dcd06f04260c9b73ba2bbfc
cl4uhu <pjclaudia1@gmail.com>
2023-09-20 11:01:54 +0200

    infinitive jumps fixed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,12 +10,13 @@ public class Player : MonoBehaviour
     //private float _playerInputVertical;
 
     private Rigidbody2D _rBody2D;
-    
+    private GroundSensor _sensor;
 
     // Start is called before the first frame update
     void Start()
     {
       _rBody2D = GetComponent<Rigidbody2D>();
+      _sensor = GetComponentInChildren<GroundSensor>();
     }
 
     void FixedUpdate()
@@ -29,7 +30,7 @@ public class Player : MonoBehaviour
     {
         PlayerMovement ();  
 
-        if(Input.GetButtonDown("Jump"))
+        if(Input.GetButtonDown("Jump") && _sensor._isGrounded)
         {
           Jump();
         }
