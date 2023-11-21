da1c569c9ab393044370b999e271049094fc3cf0
cl4uhu <pjclaudia1@gmail.com>
2023-11-21 20:15:55 +0100

    stars

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -1,14 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
+    public int _stars = 0;
+    [SerializeField] private Text _starstext;
+    [SerializeField] private AudioClip _starsound;
+    AudioSource _audiosource;
+
+    VictoryMenu _victorymenu;
+
+    void Start()
+    {
+      _audiosource = GetComponent<AudioSource>();
+      _victorymenu = GameObject.Find("VictoryMenu").GetComponent<VictoryMenu>();
+    }
+
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Star"))
         {
             Destroy(collision.gameObject);
+            _stars++;
+            _starstext.text = "Collected Stars: " + _stars + "/6";
+            _audiosource.PlayOneShot(_starsound);
+        }
+    }
+
+    void Update()
+    {
+        if(_stars == 6)
+        {
+          _victorymenu.Victory();
         }
     }
 }
--- Assets/Scripts/VictoryMenu.cs
+++ Assets/Scripts/VictoryMenu.cs
@@ -9,4 +9,9 @@ public class VictoryMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
+
+    public void Victory()
+    {
+        SceneManager.LoadScene("Victory");
+    }
 }
